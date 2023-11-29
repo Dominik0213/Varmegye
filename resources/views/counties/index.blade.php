2ef1f5a88415883152c8579dcc5424c5eb12db12
3116TAN-09 <3116TAN-09@boronkay.local>
2023-11-29 13:06:47 +0100

    frontend

--- resources/views/counties/index.blade.php
+++ resources/views/counties/index.blade.php
@@ -1,24 +1,37 @@
-<!-- resources/views/counties/index.blade.php -->
-<a href="{{ url('/') }}" title="Kezdőlap"><i class="fa fa-home"></i></a>
-<h1>Vármegyék</h1>
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vármegyék</title>
+  
+</head>
+<body style="font-family: Arial, sans-serif; margin: 20px; display: flex; flex-direction: column; align-items: center; background-color: #FCE4EC;">
 
-<a href="{{ route('counties.create') }}">Vármegye hozzáadás</a>
+<h1 style="color: #333; margin-top: 20px;">Vármegyék</h1>
 
-<form action="{{ route('counties.filter') }}" method="GET">
-    <input type="text" name="filter" placeholder="Szűrés név szerint">
-    <button type="submit">Szűrő</button>
-</form>
+<div style="display: flex; justify-content: space-between; align-items: center; width: 50%;">
+    <a href="{{ route('counties.create') }}" style="display: inline-block; padding: 10px 15px; background-color: #800080; /* Lila */ color: white; text-decoration: none; margin-bottom: 10px;">Vármegye hozzáadás</a>
 
-<ul>
+    <form action="{{ route('counties.filter') }}" method="GET" style="display: flex; align-items: center;">
+        <input type="text" name="filter" placeholder="Szűrés vármegye szerint" style="padding: 8px; margin-right: 10px;">
+        <button type="submit" style="padding: 8px; background-color: #800080; /* Lila */ color: white; border: none; cursor: pointer;">Szűrő</button>
+    </form>
+</div>
+
+<ul style="list-style-type: none; padding: 0; width: 50%;">
     @foreach ($counties as $county)
-        <li>
+        <li style="margin-bottom: 10px; border: 1px solid #ddd; padding: 10px; background-color: #f9f9f9; display: flex; justify-content: space-between; align-items: center;">
             {{ $county->name }}
-            <a href="{{ route('counties.edit', $county) }}">Szerkesztés</a>
-            <form action="{{ route('counties.destroy', $county) }}" method="POST">
+            <a href="{{ route('counties.edit', $county) }}" style="margin-left: 10px; text-decoration: none; color: #3498db;">Szerkesztés</a>
+            <form action="{{ route('counties.destroy', $county) }}" method="POST" style="display: inline-block; margin-left: 10px;">
                 @csrf
                 @method('DELETE')
-                <button type="submit">Törlés</button>
+                <button type="submit" style="background-color: #800080; /* Lila */ color: white; border: none; padding: 5px 10px; cursor: pointer;">Törlés</button>
             </form>
         </li>
     @endforeach
 </ul>
+
+</body>
+</html>
